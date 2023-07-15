6aa726dd492768f34ba618c5a2b692679ba334bd
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-15 22:04:41 +0530

    max of str

---
+++ max.java
@@ -0,0 +1,35 @@
+package Max_numsub;
+
+import java.util.Scanner;
+
+public class max {
+	public static int maximum(int arr[],int n,int b) {
+		int max=0;
+		for(int i=0;i<n;i++) {	
+			int sum=0;
+			for(int j=i;j<n;j++) 
+			{
+				sum+=arr[j];				
+				if(sum<=b && sum >max) 
+				{
+				max=sum;
+				}
+			}
+		}
+		return max;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int b=sc.nextInt();
+		int value=maximum(arr,n,b);
+		System.out.println(value);
+	}
+}
